655684a00df052cf9eb561288bd31389b76a6a73
WTsaur <william.tsaur@gmail.com>
2020-09-27 13:18:01 -0700

    fixed printing bug; part 2 is complete

--- part2/my_timer.c
+++ part2/my_timer.c
@@ -24,17 +24,24 @@ static ssize_t read(struct file* file, char *ubuf, size_t count, loff_t *ppos) {
     if (*ppos > 0 || cout < len)
         return 0;
     
-    len += sprintf(buf, "current time: %li.%li\n", currTime.tv_sec, currTime.tv_nsec);
+    len += sprintf(buf, "current time: %li.%09%li\n", currTime.tv_sec, currTime.tv_nsec);
     
     if (prevTime.tv_sec > -1 && prevTime.tv_nsec > -1) {
         long int elap_sec = currTime.tv_sec - prevTime.tv_sec;
         long int elap_nsec;
-
-        if (prevTime.tv_nsec > currTime.tv_nsec)
-            elap_nsec = 999999999 - prevTime.tv_nsec + currTime.tv_nsec;
-        else
+        
+        if (prevTime.tv_nsec > currTime.tv_nsec) {
+            
+            if (currTime.tv_sec - prevTime.tv_sec == 1)
+                elap_sec = 0;
+            
+            elap_nsec = 1000000000 - prevTime.tv_nsec + currTime.tv_nsec;
+        
+        } else
+        
             elap_nsec = currTime.tv_nsec - prevTime.tv_nsec;
-        len += spritnf(buf, "elapsed time: %li.%li\n", elap_sec, elap_nsec);
+        
+        len += spritnf(buf + len, "elapsed time: %li.%09%li\n", elap_sec, elap_nsec);
     }
 
     prevTime.tv_sec = currTime.tv_sec;
