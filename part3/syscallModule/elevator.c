a973bbf18df803a021a1359368bb11ef5d152bef
Nicholas Tidwell <nbt16b@my.fsu.edu>
2020-10-01 18:21:50 -0400

    Add Ability for elivator to move up and down

--- part3/syscallModule/elevator.c
+++ part3/syscallModule/elevator.c
@@ -8,11 +8,14 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
 #include <linux/list.h>
+#include <linux/delay.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("DUAL BSD/GPL");
 
 void addToQueue(int, int, int);
 void print_elevator(void);
+void change_floor(int);
 
 #define NUM_FLOORS 10
 
@@ -36,12 +39,12 @@ static int procfs_buf_len;
 int curr_floor;
 int next_floor;
 int elevator_state;
-int elevator_next_dir;
+// int elevator_direction;
 int num_passenger;
 int passenger_waiting;
 int	infected_status;
 int passenger_served;
-
+int stop_signal = 0;
 typedef struct queue_member {
 
 	struct list_head list;
@@ -50,12 +53,71 @@ typedef struct queue_member {
 	int dest_floot;
 } queue_member;
 
+
+struct task_struct *elevator_thread;
+
 struct list_head passenger_queue[NUM_FLOORS]; //queue of passenger
 struct list_head elevator_list; //queue of elevator
 
+
+static int run_elevator(void *data){
+
+	while(!kthread_should_stop()){
+		ssleep(1);
+		switch(elevator_state){
+			case OFFLINE:
+				printk(KERN_INFO "Elevator OFFLINE");
+				break;
+			case IDLE:
+				printk(KERN_INFO "Elevator IDLE");
+				//Check to Unload
+				elevator_state = UP;
+				next_floor = curr_floor + 1;
+				break;
+			case UP:
+				change_floor(next_floor);
+				printk(KERN_INFO "Elevator UP");
+				if(curr_floor == 10){
+					elevator_state = DOWN;
+					next_floor = curr_floor - 1;
+				}
+				else next_floor = curr_floor + 1;
+				break;
+			case DOWN:
+				change_floor(next_floor);
+				printk(KERN_INFO "Elevator Down");
+				if(curr_floor == 1){
+					elevator_state = UP;
+					next_floor = curr_floor + 1;
+				}
+				else next_floor = curr_floor - 1;
+
+				if(stop_signal == 1 && curr_floor == 1) //TODO: Add Check to make sure no one is left to serve in the future
+				{
+					elevator_state = OFFLINE;
+					stop_signal = 0;
+				}
+				break;
+			case LOADING:
+				printk(KERN_INFO "Elevator Loading");
+				break;
+		}
+	}
+	printk(KERN_INFO "Thread Stopping\n");
+	return 0;
+}
+
+void change_floor(int floor){
+
+	if(floor != curr_floor){
+		ssleep(2);
+		curr_floor = floor;
+	}
+}
+
 static ssize_t elevator_read(struct file* file, char * ubuf, size_t count, loff_t *ppos)
 {
-	printk(KERN_INFO "Elevator State: %d\n Elevator Satus: %d\n Current Floor: %d\n  Number of Passengers: %d\n Number or Passengers Waiting: %d\n Number Passengers Serviced: %d\n", elevator_state, elevator_state, curr_floor, num_passenger, passenger_waiting, passenger_served);
+	printk(KERN_INFO "Elevator State: %d\n Elevator Satus: %d\n Current Floor: %d\n  Number of Passengers: %d\n Number or Passengers Waiting: %d\n Number Passengers Serviced: %d\n", elevator_state, infected_status, curr_floor, num_passenger, passenger_waiting, passenger_served);
 	print_elevator();
 	procfs_buf_len = strlen(msg);
 
@@ -68,10 +130,9 @@ static ssize_t elevator_read(struct file* file, char * ubuf, size_t count, loff_
 	return procfs_buf_len;
 
 }
-
-
 static ssize_t elevator_write(struct file* file, const char * ubuf, size_t count, loff_t* ppos)
 {
+	//Elevator Write is Not needed for this project...
 	printk(KERN_INFO "elevator proc_write test\n");
 	if (count > BUF_LEN)
 		procfs_buf_len = BUF_LEN;
@@ -89,9 +150,12 @@ static struct file_operations procfile_fops = {
 };
 extern long (*STUB_start_elevator)(void);
 long start_elevator(void) {
-	printk("Start Request");
-	//Implement Start Elevator Below
-	return 1;
+	if(elevator_state == OFFLINE){
+		printk(KERN_INFO "Starting Elevator\n");
+		elevator_state = IDLE;
+		return 1;
+	}
+	return 0;
 }
 
 extern long (*STUB_issue_request)(int,int,int);
@@ -112,10 +176,16 @@ long issue_request(int start, int stop, int type) {
 extern long (*STUB_stop_elevator)(void);
 long stop_elevator(void) {
 	printk("Stop Request");
+	if(stop_signal == 0){
+		stop_signal = 1;
+		return 0;
+	}
 	return 1;
 }
 
 static int elevator_init(void) {
+	printk(KERN_ALERT "Elevator INIT\n");
+
 	STUB_start_elevator = start_elevator;
 	STUB_stop_elevator = stop_elevator;
 	STUB_issue_request = issue_request;
@@ -137,14 +207,26 @@ static int elevator_init(void) {
 	int	infected_status = UNINFECTED;
 	int passenger_served = 0;
 	passenger_waiting = 0;
-	printk(KERN_ALERT "Elevator INIT\n");
+
+	printk(KERN_ALERT "Elevator Thread Cretae\n");
+
+	//Initialize Elevator Thread
+	 elevator_thread = kthread_run(run_elevator, NULL, "mythread");
+    // if(IS_ERR(elevator_thread)) {
+    //   printk("Error: ElevatorRun\n");
+    //   return PTR_ERR(elevator_thread);
+    // }
+	// else
+	// 	printk(KERN_INFO "Thread Created Successfully\n");c
+
+
+	//Initialize Proc Entry
 	proc_entry = proc_create("elevator", 0666, NULL, &procfile_fops);
 
 	if(proc_entry == NULL)
 	{	
 		 return -ENOMEM;
 	}
-	printk(KERN_ALERT "Elevator INIT Exit\n");
 
 	return 0;
 }
@@ -154,6 +236,8 @@ static void elevator_exit(void) {
 	STUB_stop_elevator = NULL;
 	STUB_issue_request = NULL;
 	printk(KERN_ALERT "Elevator EXIT\n");
+	if(elevator_thread)
+		kthread_stop(elevator_thread);
 	proc_remove(proc_entry);
 	return;
 }
